d9d87e9978c451289c3ae9d0b07f2714bbdc7a97
Vignesh Hari <vichuhari100@gmail.com>
2018-10-19 21:50:36 +0530

    Started working on it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Typography from '@material-ui/core/Typography';
+
+class App extends Component {
+
+
+  componentDidMount(){
+
+  }
+
+
+
+render() {
+
+  return (
+
+    <Typography  align="center" variant="overline" gutterBottom   classes={{
+    root: 'classes-state-heading test isgradplain', }
+   }>
+      Cute Weather App
+    </Typography>
+
+)
+}
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
