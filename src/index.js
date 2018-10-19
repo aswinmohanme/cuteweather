561e0351881e843753cf1c356ef898feae4f6021
Vignesh Hari <vichuhari100@gmail.com>
2018-10-19 22:33:43 +0530

    Added Openweathermap API data

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+
+
+var apiKey = "ae754027ab44ce684e50d36881b26d0f";
 
 class App extends Component {
 
 
   componentDidMount(){
-
-  }
+    let city = "kochi";
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&apiKey=${apiKey}`)
+         .then(data => {
+           console.log(data.data);
+         }
+);
+}
 
 
 
